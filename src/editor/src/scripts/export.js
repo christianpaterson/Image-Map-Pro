cc93ba2b71f0755bfbcc97a6d3f1a828966947f0
Chris <cpaterson152@gmail.com>
2023-06-27 13:16:25 -0700

    Initial commit

---
+++ src/editor/src/scripts/export.js
@@ -0,0 +1,35 @@
+import * as utilities from 'Editor/scripts/utilities'
+import * as defaults from 'Client/scripts/defaults'
+import { version } from '../../version'
+
+export function exportSettings(settings) {
+  let artboards = []
+
+  for (let artboard of settings.artboards) {
+    let compressedArtboard = utilities.deepExtend({}, utilities.subtract(artboard, defaults.artboardDefaults))
+    compressedArtboard.children = compressObjects(artboard.children)
+    compressedArtboard = utilities.clean(compressedArtboard) || {}
+    artboards.push(compressedArtboard)
+  }
+
+  let compressed = utilities.deepExtend({}, utilities.clean(utilities.subtract(settings, defaults.imageMapDefaults)))
+  compressed.artboards = artboards
+  compressed.version = version
+  compressed.general = compressed.general || {}
+  compressed.general.name = settings.general.name
+  return compressed
+}
+
+function compressObjects(objects) {
+  let result = []
+
+  if (objects) {
+    for (let object of objects) {
+      let compressedObject = utilities.deepExtend({}, utilities.clean(utilities.subtract(object, defaults.objectDefaults)))
+      compressedObject.children = compressObjects(object.children)
+      result.push(compressedObject)
+    }
+  }
+
+  return result
+}
